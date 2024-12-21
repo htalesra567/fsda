f96c9c97b320bac3480c57b16453f88eb14488c0
hitesh <htalesra567@gmail.com>
2024-12-21 17:53:31 +0530

    afdsf

---
+++ Solution.java
@@ -0,0 +1,15 @@
+
+Given the head of a signly linked list reverse the list and return reversed list 
+
+
+4. intersection of 2 arrays must be unique elements
+
+given a string column title that represents the column title as appears in an excel sheet retrun its corresponding column number
+A->1; B->2 .....  AA->27 AB->28
+
+6. return index of element if found in array otherwise return index where it would be inserted
+
+valid anagram 
+return true if two strings have same alphabets
+
+5. move all zeroes at the end of an array without disrupting the order
